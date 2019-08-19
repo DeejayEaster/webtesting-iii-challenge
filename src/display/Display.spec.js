cca00520d528b8a1756fa1b0c4d01a3f71517d33
deejayeaster <deejayeaster@live.com>
2019-08-19 18:01:46 -0400

    made display test

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,17 @@
-// Test away!
+import React from "react";
+import { render } from "@testing-library/react";
+import Display from "./Display";
+import "react-testing-library/cleanup-after-each";
+
+describe("<Display />", () => {
+  it("Default state set to open and unlocked", () => {
+    const { getByText } = render(<Display />);
+    expect(getByText("Open"));
+    expect(getByText("Unlocked"));
+  });
+  it("Accepting props", () => {
+    const { getByText } = render(<Display locked={true} closed={true} />);
+    expect(getByText("Closed"));
+    expect(getByText("Locked"));
+  });
+});
